ca0eed5a388c8f62a8e70b5e456564944cfef3c8
BachoOnn <oniani.bachoo@gmail.com>
2026-01-09 20:41:12 +0400

    add helpers

---
+++ Swish/Helpers/Common/LoadingState.swift
@@ -0,0 +1,13 @@
+//
+//  LoadingState.swift
+//  Auth
+//
+//  Created by Bacho on 04.01.26.
+//
+
+enum LoadingState {
+    case idle
+    case loading
+    case success
+    case error(String)
+}
---
+++ Swish/Helpers/ScreenSize.swift
@@ -0,0 +1,14 @@
+//
+//  ScreenSize.swift
+//  Auth
+//
+//  Created by Bacho on 01.01.26.
+//
+
+
+import UIKit
+
+struct ScreenSize {
+    static let width = UIScreen.main.bounds.width 
+    static let height = UIScreen.main.bounds.height
+}
