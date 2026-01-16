f192034e6a25413131d4bdae8b94ab845d5ccf73
BachoOnn <oniani.bachoo@gmail.com>
2026-01-17 03:26:15 +0400

    add extension for config, update info.plist

---
+++ Config/Config.swift
@@ -0,0 +1,29 @@
+//
+//  Config.swift
+//  Swish
+//
+//  Created by Bacho on 17.01.26.
+//
+
+import Foundation
+
+public protocol Config {
+    var DEBUG: Bool { get }
+    var BALLDONTLIE_API_KEY: String { get }
+    var BALLDONTLIE_BASE_URL: String { get }
+}
+
+extension Bundle: Config {
+    
+    public var DEBUG: Bool {
+        object(forInfoDictionaryKey: "DEBUG") as! Bool
+    }
+    
+    public var BALLDONTLIE_API_KEY: String {
+        object(forInfoDictionaryKey: "BALLDONTLIE_API_KEY") as! String
+    }
+    
+    public var BALLDONTLIE_BASE_URL: String {
+        object(forInfoDictionaryKey: "BALLDONTLIE_BASE_URL") as! String
+    }
+}
